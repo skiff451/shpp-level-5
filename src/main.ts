0fd038fb082f492a8413d3dd699cbe44aafee7bf
Serghey <samsomskiff451@gmail.com>
2021-01-28 23:09:25 +0200

    finish timer logic

---
+++ src/main.ts
@@ -0,0 +1,65 @@
+// import moment from 'moment';
+import * as moment from 'moment';
+
+const time = document.querySelector('#time');
+const leftBtn = document.querySelector('#minus');
+const rightBtn = document.querySelector('#plus');
+const startBtn = document.querySelector('#start-button');
+const label = document.querySelector('#label');
+
+console.log(label);
+
+leftBtn.addEventListener('click', changeTimer(time, false));
+rightBtn.addEventListener('click', changeTimer(time));
+startBtn.addEventListener('click', () => {
+    startTimer(time, label);
+})
+
+function changeTimer(timeNode: Element, incr: boolean = true) {
+    if (incr) {
+        return () => {
+            let changeItem: number = +timeNode.innerHTML;
+            changeItem++;
+            timeNode.innerHTML = changeItem.toString();
+        }
+    }
+    else {
+        return () => {
+            let changeItem: number = +timeNode.innerHTML;
+            if (changeItem != 0) {
+                changeItem--;
+                timeNode.innerHTML = changeItem.toString();
+            }
+        }
+    }
+}
+
+function startTimer(timeNode: Element, labelNode: Element): void {
+    const duration = moment.duration({
+        seconds: 0,
+        minutes: +timeNode.innerHTML,
+    });
+
+    labelNode.innerHTML = 'Осталось:';
+
+    toggleHideBtns(leftBtn, rightBtn, startBtn);
+
+    const onTimer = setInterval(() => {
+        duration.subtract(1, 'seconds');
+        
+        timeNode.innerHTML = `${duration.minutes()}:${duration.seconds()}`
+        
+        if (duration.minutes() === 0 && duration.seconds() === 0) {
+            clearInterval(onTimer);
+            toggleHideBtns(leftBtn, rightBtn, startBtn);
+            labelNode.innerHTML = 'Укажите время в минутах';
+            timeNode.innerHTML = '0';
+        }
+    }, 1000)
+}
+
+function toggleHideBtns(...btns : Element[]) {
+    btns.forEach(item => {
+        item.classList.toggle('hide');
+    })
+}
